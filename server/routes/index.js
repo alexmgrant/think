9ac5f239e08c3c9d425372dcaa32f3d235e243cd
Alex Grant <alexmgrant@me.com>
2020-11-13 21:35:11 -0500

    removing unused import

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import passport from 'passport';
 import dotenv from 'dotenv';
-import ensureAuth from 'connect-ensure-login';
 
 import * as identifier from './routes.identifier.js';
 
