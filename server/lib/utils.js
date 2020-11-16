aad69f3793491a0a3619d7a5d3388801c8e36b11
Alex Grant <alexmgrant@me.com>
2020-11-16 11:35:20 -0500

    fixing bug where integer 0 was not handled

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,7 +1,7 @@
 export const hasPayload = (body) => body !== undefined;
 export const isNumber = (body) => typeof body === 'number';
 export const isNegativeNumber = (number) =>
-  isNumber(number) && Math.sign(number) !== 1;
+  isNumber(number) && Math.sign(number) !== 1 && Math.sign(number) !== 0;
 
 export const handleRes = (status) => (res) => res.status(status);
 export const handle400 = handleRes(400);
